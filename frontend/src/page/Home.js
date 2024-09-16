a3c1568e5fa99e41934cbac1a9fca39e45cc1aa5
HyperLeafy <100145556+HyperLeafy@users.noreply.github.com>
2024-09-16 20:31:57 +0530

    fix lets talk button

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../component/navbar/navbar';
 import '../styles/home.css';
 import ParticleEffect from '../component/background/particel_effect_1';
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="wrapper">
       <SideBar />
@@ -12,7 +13,7 @@ function Home() {
         <section className="box">
           <h2>Hello! I'm a software developer with a knack for innovative design</h2>
           <p>You can call me Leafy. I like to convert thoughts into reality. I’m always up for a tech adventure and figuring out how to make things work in unique ways.</p>
-          <button id="button" onClick={() => alert('Button Clicked')}>
+          <button id="button" onClick={() =>navigate('/contact')}>
             Let's Talk
           </button>
         </section>
